9bd564679e2f8f0606ab9f49890899f61d2b3adb
Taffy <taffy@enclave.vn>
2021-01-08 09:09:13 +0700

    .env variable environment

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const tourRouter = require('./routes/tourRouter')
 const userRouter = require('./routes/userRouter')
 
 const app = express()
-
-app.use(morgan('dev'))
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
 app.use(express.json())
 app.use(express.static(`${__dirname}/public/`))
 
