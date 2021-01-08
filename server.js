15b4f9759fed0ed7a8832370485947bdb1655341
Taffy <taffy@enclave.vn>
2021-01-08 14:33:20 +0700

    Configure mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
 dotenv.config({ path: './config.env' })
 
 const app = require('./app')
 
-const PORT = process.env.PORT
+const { PORT, DATABASE_URI, DATABASE_PASSWORD } = process.env
+const connectionString = DATABASE_URI.replace('<PASSWORD>', DATABASE_PASSWORD)
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected')
+  })
+
 app.listen(PORT, () => {
   console.log(`App is running at http://localhost:${PORT}`)
 })
