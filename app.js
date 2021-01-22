85754b4887fb1f4f551665c57df009502abe3133
Taffy <taffy@enclave.vn>
2021-01-22 09:41:31 +0700

    Create global error handler and error middleware

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
 const morgan = require('morgan')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+
 const tourRouter = require('./routes/tourRouter')
 const userRouter = require('./routes/userRouter')
 
 const app = express()
-console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
@@ -14,5 +15,17 @@ app.use(express.static(`${__dirname}/public/`))
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+//Add route above this line
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Not found',
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // })
+  const error = new AppError(`Cant find ${req.originalUrl} on this server`, 404)
+  next(error)
+})
+
+app.use(globalErrorHandler)
 
 module.exports = app
