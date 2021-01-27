7ede0b60ceadb6202eb138d80a3093b6147117be
Taffy <taffy@enclave.vn>
2021-01-27 10:39:07 +0700

    review model

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must be belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must be belong to a user'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = Review
